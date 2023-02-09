f6eb6e81e6c0af707392e96db5928ebc4bb70050
lunleo <luna.lenya@gmail.com>
2023-02-08 20:41:11 -0500

    Update Sentence.cs

--- english-learning-application/Models/Sentence.cs
+++ english-learning-application/Models/Sentence.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Collections;
+
 namespace english_learning_application.Models
 {
 	public class Sentence
 	{
 		public Sentence()
 		{
+			contextIds = new ArrayList();
         }
 
-		//-id
-//-contextIds
-//-Sentence
-//-ovnerId
-//-word
-//-originalSentence
+		public Sentence(int id, ArrayList contextIds, string sentence, int ownerId, string word, string originalSentence)
+		{
+			Id = id;
+			this.contextIds = contextIds;
+			this.sentence = sentence;
+			this.ownerId = ownerId;
+			this.word = word;
+			this.originalSentence = originalSentence;
+		}
 
-    }
+		public int Id { get; set; }
+		public ArrayList contextIds { get; set; }
+		public string sentence { get; set; }
+		public int ownerId { get; set; }
+		public string word { get; set; }
+		public string originalSentence { get; set; }
+
+	}
 }
 
