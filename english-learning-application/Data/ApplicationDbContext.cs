da116350df6562a4adfc772c5a63ab56cf9e978e
lunleo <luna.lenya@gmail.com>
2023-03-12 16:19:01 -0400

    Update ApplicationDbContext.cs

--- english-learning-application/Data/ApplicationDbContext.cs
+++ english-learning-application/Data/ApplicationDbContext.cs
@@ -56,7 +56,55 @@ public class ApplicationDbContext : IdentityDbContext
             .HasIndex(context => context.Name)
             .IsUnique();
 
+		//Test
+		builder.Entity<Test>()
+		 .HasMany<Word>(test => test.Words)
+		 .WithMany(test => test.Tests);
 
-    }
+		builder.Entity<Test>()
+			.HasOne<Language>(test => test.Language)
+			.WithMany(language => language.Tests)
+			.HasForeignKey(test => test.LanguageId);
+
+		//TranslatedSentence
+		//builder.Entity<TranslatedSentence>()
+		//    .HasMany<Context>(translatedSentence => translatedSentence.Contexts)
+		//    .WithMany(context => context.TranslatedSentences);
+
+		builder.Entity<TranslatedSentence>()
+			.HasOne<Word>(translatedSentence => translatedSentence.Word)
+			.WithMany(word => word.TranslatedSentences)
+			.HasForeignKey(translatedSentence => translatedSentence.WordId);
+
+		//builder.Entity<TranslatedSentence>()
+		//    .HasOne<Sentence>(translatedSentence => translatedSentence.Sentence)
+		//    .WithMany(sentence => sentence.TranslatedSentences)
+		//    .HasForeignKey(translatedSentence => translatedSentence.SentenceId);
+
+		builder.Entity<TranslatedSentence>()
+			.HasOne<Language>(translatedSentence => translatedSentence.Language)
+			.WithMany(language => language.TranslatedSentences)
+			.HasForeignKey(translatedSentence => translatedSentence.LanguageId);
+
+		builder.Entity<TranslatedSentence>()
+		  .HasIndex(translatedSentence => translatedSentence.Sentence)
+		  .IsUnique();
+
+		//TranslatedWord
+		//builder.Entity<TranslatedWord>()
+		//    .HasMany<Context>(translatedWord => translatedWord.Contexts)
+		//    .WithMany(context => context.TranslatedWords);
+
+
+
+		builder.Entity<TranslatedWord>()
+			.HasOne<Language>(translatedWord => translatedWord.Language)
+			.WithMany(language => language.TranslatedWords)
+			.HasForeignKey(translatedWord => translatedWord.LanguageId);
+
+		builder.Entity<TranslatedWord>()
+			.HasIndex(translatedWord => translatedWord.Translation)
+			.IsUnique();
+	}
 }
 
