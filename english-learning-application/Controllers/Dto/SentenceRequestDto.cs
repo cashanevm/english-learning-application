55aa65a061d286d960ba303841151d9f56192dba
Kateryna Shmat <katushkashmat@gmail.com>
2023-03-23 20:55:00 +0200

    controllers_by_kateryna

---
+++ english-learning-application/Controllers/Dto/SentenceRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace english_learning_application.Controllers.Dto
+{
+	public class SentenceRequestDto
+	{
+		public SentenceRequestDto()
+        {
+            ContextIds = new List<int>();
+        }
+
+        //many Sentence to many Context
+        public List<int> ContextIds { get; set; }
+
+        public int OwnerId { get; set; }
+
+        //foreign key
+        //many Sentence to one Word
+        public int WordId { get; set; }
+
+        //unic key
+        public string OriginalSentence { get; set; }
+    }
+}
+
---
+++ english-learning-application/Controllers/TagController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using english_learning_application.Controllers.Dto;
+using english_learning_application.Data;
+using english_learning_application.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace english_learning_application.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tag
+        [HttpGet]
+        public IEnumerable<TagResponseDto> GetTags()
+        {
+            return _context.Tags
+                .Include(l => l.Words)
+                .ToList().Select(x => new TagResponseDto(x));
+        }
+
+        // GET: api/Tag/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TagResponseDto>> GetTag(int id)
+        {
+            var tag = _context.Tags
+                .Include(l => l.Words)
+                .FirstOrDefault(x => x.ID == id); ;
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return new TagResponseDto(tag);
+        }
+
+        // PUT: api/Tag/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTag(int id, TagRequestDto dto)
+        {
+            Tag tag = new Tag();
+
+            tag.Name = dto.Name;
+            tag.ID = id;
+
+            _context.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tag
+        [HttpPost]
+        public async Task<ActionResult<Tag>> PostTag(TagRequestDto dto)
+        {
+
+            Tag tag = new Tag();
+
+            tag.Name = dto.Name;
+       
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTag", new { id = tag.ID }, tag);
+        }
+
+        // DELETE: api/Tag/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTag(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Tags.Any(e => e.ID == id);
+        }
+    }
+}
+
