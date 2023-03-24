89548ebe4b7f7a4e270e1c34d60c88537741c9d3
nbboutique <46777706+nbboutique@users.noreply.github.com>
2023-03-24 08:17:21 +0100

    Add files via upload

---
+++ english-learning-application/Controllers/Dto/TranslatedWordRequestDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using english_learning_application.Models;
+
+namespace english_learning_application.Controllers.Dto
+{
+	public class TranslatedWordRequestDto
+	{
+		public TranslatedWordRequestDto()
+        {
+            ContextIds = new List<int>();
+        }
+
+        //many TranslatedWord to many Contexts
+        public List<int> ContextIds { get; set; }
+
+        public int OwnerId { get; set; }
+
+        //foreign key
+        //many TranslatedWord to one Word
+        public int WordId { get; set; }
+
+        //unic key
+        public string Translation { get; set; }
+
+        //foreign key
+        //many TranslatedWord to one Language
+        public int LanguageId { get; set; }
+
+        public int SpeechPartId { get; set; }
+    }
+}
+
